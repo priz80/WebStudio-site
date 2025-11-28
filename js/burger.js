9155526854a1827a98156d194a33d0405d621d7f
priz80 <priz8090@gmail.com>
2025-11-28 09:58:20 +0300

    Update burger.js

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,12 @@
-burger = document.getElementById('burger');
-menu = document.getElementById('menu'),
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    menu.classList.toggle('active');
+burger_1 = document.getElementById('burger-1');
+burger_2 = document.getElementById('burger-2');
+menu_1 = document.getElementById('menu-1');
+menu_2 = document.getElementById('menu-2');
+burger_1.addEventListener('click', () => {
+    burger_1.classList.toggle('active');
+    menu_1.classList.toggle('active');
+});
+burger_2.addEventListener('click', () => {
+    burger_2.classList.toggle('active');
+    menu_2.classList.toggle('active');
 });
